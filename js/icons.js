41488dd1e1b96258c581a28cee7db7ec8b0c0672
leticiabeatriz <43241810+leticiabeatriz@users.noreply.github.com>
2026-08-08 12:12:23 -0300

    v2

--- js/icons.js
+++
@@ -1,2 +0,0 @@
-const exceptions={"Shepherd's Pie":"Shepherds Pie.png","Caffè Latte":"Caffe Latte.png","Caffè Mocha":"Caffe Mocha.png"};
-export function iconUrl(nameEn){if(!nameEn)return"";const fileName=exceptions[nameEn]??`${nameEn}.png`;return`https://hayday.fandom.com/wiki/Special:Redirect/file/${encodeURIComponent(fileName)}`}
